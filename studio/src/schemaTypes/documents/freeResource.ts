6ed8e74a92161080466aaf527d12f64793282beb
razvy.dev <razvan.andronachi2007@gmail.com>
2026-06-22 17:56:58 +0300

    chore: update the validation rules and add some needed fields

--- studio/src/schemaTypes/documents/freeResource.ts
+++ studio/src/schemaTypes/documents/freeResource.ts
@@ -38,10 +38,42 @@ export const freeResource = defineType({
             validation: (Rule) => Rule.required(),
         }),
         defineField({
+            name: "cta",
+            title: "Call to Action",
+            type: "string",
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
             name: 'file',
             title: 'File',
             description: 'Upload the free resource file here. This file can be: a PDF, an image, or an HTML file (rendered through an IFrame).',
-            type: 'file',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'type',
+                    title: 'File Type',
+                    type: 'string',
+                    options: {
+                        list: [
+                            {title: 'PDF', value: 'pdf'},
+                            {title: 'Image', value: 'image'},
+                            {title: 'HTML (IFrame)', value: 'html'},
+                        ],
+                    },
+                    validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                    name: 'asset',
+                    title: 'Asset',
+                    type: 'file',
+                })
+            ]
+        }),
+        defineField({
+            name: 'externalLink',
+            title: 'External Link',
+            description: 'Alternatively, provide an external URL for the resource. This will override the file upload if both are provided.',
+            type: 'url',
         }),
         defineField({
             name: 'pageBuilder',
